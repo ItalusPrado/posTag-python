eeb603b1ad511b280658a49b882b783c45f5dd85
gabrielsscavalcante <gabrielsoucavalcante@gmail.com>
2017-10-10 00:34:34 -0300

    Trying model.

--- Data.py
+++ Data.py
@@ -1,18 +1,26 @@
 class Data():
 
-    probWords = dict()
+    probabilityWords = dict()
+    probabilityWordsLabels = dict()
     totalWords = 0
 
     def addCountWord(self,word):
         self.totalWords += 1
-        if not self.probWords.__contains__(word):
-            self.probWords[word] = 1
+        if not self.probabilityWords.__contains__(word):
+            self.probabilityWords[word] = 1
         else:
-            self.probWords[word] = self.probWords[word]+1
+            self.probabilityWords[word] = self.probabilityWords[word]+1
 
 
     def probForWords(self):
-        for word in self.probWords:
-            self.probWords[word] = self.probWords[word]/self.totalWords
-            print(self.probWords[word])
+        for word in self.probabilityWords:
+            self.probabilityWords[word] = self.probabilityWords[word]/self.totalWords
 
+
+    def probabilityForLabels(self, words, labels):
+        for word in words:
+            probabilityLabels = dict()
+            for label in labels:
+                #calcular a probabilidade das labels
+                probabilityLabels[label] = 0.2
+            self.probabilityWordsLabels[word] = probabilityLabels
