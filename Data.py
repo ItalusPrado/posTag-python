3aa01f1fdb57a50f9853f0f5c973bb1c0ea1b536
ItalusPrado <italusprado@acad.ifce.edu.br>
2017-10-10 02:55:54 -0300

    created: count of label in word, probability of label in word

--- Data.py
+++ Data.py
@@ -4,8 +4,10 @@ class Data():
     probabilityWordsLabels = dict()
 
     countWordLabels = dict()
+
     totalWords = 0
 
+    # Talvez seja inutil
     def addCountWord(self,word):
         self.totalWords += 1
         if not self.probabilityWords.__contains__(word):
@@ -13,22 +15,34 @@ class Data():
         else:
             self.probabilityWords[word] = self.probabilityWords[word]+1
 
-
+    # Talvez seja inutil
     def probForWords(self):
         for word in self.probabilityWords:
             self.probabilityWords[word] = self.probabilityWords[word]/self.totalWords
 
 
     def countLabelForWord(self,word,label):
-        if not self.countWordLabels.has_key(word):
-            internDict[label] = 1
-            self.countWordLabels[word] = internDict
+        if word in self.countWordLabels:
+            if label in self.countWordLabels[word]:
+                self.countWordLabels[word][label] = self.countWordLabels[word][label]+1
+            else:
+                self.countWordLabels[word][label] = 1
+        else:
+            self.countWordLabels[word] = {label: 1}
 
+    def probabilityLabelForWord(self):
+        for word in self.countWordLabels:
+            total = self.probabilityWords[word]
+            for label in self.countWordLabels[word]:
+                if word in self.probabilityWordsLabels:
+                        self.probabilityWordsLabels[word][label] = self.countWordLabels[word][label]/total
+                else:
+                    self.probabilityWordsLabels[word] = {label: self.countWordLabels[word][label]/total}
 
-    def probabilityForLabels(self, words, label):
-        for word in words:
-            probabilityLabels = dict()
-            for label in labels:
-                #calcular a probabilidade das labels
-                probabilityLabels[label] = 0.2
-            self.probabilityWordsLabels[word] = probabilityLabels
+    # def probabilityForLabels(self, words, label):
+    #     for word in words:
+    #         probabilityLabels = dict()
+    #         for label in labels:
+    #             #calcular a probabilidade das labels
+    #             probabilityLabels[label] = 0.2
+    #         self.probabilityWordsLabels[word] = probabilityLabels
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from Data import Data
 
-train = open("train.txt")
+train = open("train.txt", encoding="utf8")
 database = Data()
 
 trainTexts = [] # Array com cada texto
@@ -17,8 +17,18 @@ def setProbWord(file):
             separated = word.split('_')
             database.addCountWord(separated[0].lower())
 
+def setLabelCount(file):
+    for text in file:
+        words = text.split(' ')
+        for word in words:
+            separated = word.split('_')
+            database.countLabelForWord(separated[0].lower(),separated[1].lower())
 
 
-separateLines(train,trainTexts)
-setProbWord(trainTexts)
-database.probForWords()
+separateLines(train,trainTexts) # Separando arquivo
+setProbWord(trainTexts) # Calculando a contagem de cada palavra (pode ser útil)
+setLabelCount(trainTexts) # Calculando a contagem das labels de cada palavra
+database.probabilityLabelForWord()
+print(database.probabilityWordsLabels["a"])
+# print(database.countWordLabels['jersei'])
+# print(len(database.countWordLabels))
