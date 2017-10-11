dd6720ecc7c83664d6062f4e07035ec1b86690a4
ItalusPrado <italusprado@acad.ifce.edu.br>
2017-10-11 17:42:21 -0300

    starting to create the tree

--- HMM.py
+++ HMM.py
@@ -1,18 +1,24 @@
+from Node import Node
+
 class HMM():
 
     text = None
     word = []
     tag = []
+    nodes = []
 
-    # P(Y/Y-1)
+    tags = ["Empty","V","Subs","N"]
+
     probTests1 = [[0.2,0.4,0.4], # P(t/vazio)
                   [0.3,0.3,0.4], # P(t/verbo)
                   [0,5,0.1,0.4], # P(t/subs)
                   [0.1,0.6,0.2]] # P(t/prep)
-    # probTests2 =
+
+    probTests2 = [[0.0,0.7,0.3], # Palavra 1 = verbo 0.0 ; subs 0.7 ; prep 0.3
+                  [0.6,0.2,0.2]] # Palavra 1 = verbo 0.6 ; subs 0.2 ; prep 0.2
 
     def __init__(self,text):
-        self.text = text.rstrip()
+        self.text = "ela_N corre_V" #text.rstrip()
         self.separateText()
 
     def separateText(self):
@@ -21,11 +27,10 @@ class HMM():
             print(separated)
             self.word.append(separated[0])
             self.tag.append(separated[1])
-# =======
-# import Data as Data
-#
-# class HMM():
-#
-#     bigrams = dict()
-# >>>>>>> 461ba400f1e4f13926ea366cd9e81067d3032eb3
+
+    def startSearch(self):
+        node = Node(None,self.tag[0],1,1)
+        for index in range(0,len(self.probTests1[0])):
+            
+
 
--- Node.py
+++ Node.py
@@ -1,6 +1,14 @@
 class Node():
 
     father = None
+    tag = None
+    currentValue = None
 
-    def addFather(self):
-        self.father = Node()
+    def __init__(self,father,tag,value,cumulated):
+        self.father = father
+        self.tag = tag
+        self.currentValue = value*cumulated
+
+    def expand(self,tag,value):
+        node = Node(self,tag,value,self.currentValue)
+        return node
